6190246cd13ef54def151029a13a8feb2b36fbd7
KENZ <KENZ.gelsoft@gmail.com>
2015-06-10 01:32:21 +0900

    Tutorial step 3

--- src/Tutorial1.c
+++ src/Tutorial1.c
@@ -3,6 +3,20 @@
 static Window *s_main_window;
 static TextLayer *s_time_layer;
 
+static void update_time() {
+  time_t temp = time(NULL);
+  struct tm *tick_time = localtime(&temp);
+  
+  static char buffer[] = "00:00";
+  if (clock_is_24h_style()) {
+    strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
+  } else {
+    strftime(buffer, sizeof("00:00"), "%I:%M", tick_time);
+  }
+  
+  text_layer_set_text(s_time_layer, buffer);
+}
+
 static void main_window_load(Window *window) {
   s_time_layer = text_layer_create(GRect(0, 55, 144, 50));
   text_layer_set_background_color(s_time_layer, GColorClear);
@@ -21,6 +35,10 @@ static void main_window_unload(Window *window) {
   text_layer_destroy(s_time_layer);
 }
 
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_time();
+}
+
 static void init() {
   s_main_window = window_create();
   
@@ -30,6 +48,9 @@ static void init() {
   });
   
   window_stack_push(s_main_window, true);
+  
+  update_time();
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
 }
 
 static void deinit() {
