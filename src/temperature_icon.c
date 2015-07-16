41430576dfa055dfa79f8a6ec16760a918025d1f
KENZ <KENZ.gelsoft@gmail.com>
2015-07-17 02:00:52 +0900

    Adjust temperature label position

--- src/temperature_icon.c
+++ src/temperature_icon.c
@@ -4,8 +4,9 @@
 
 #define TEMPERATURE_FONT              FONT_KEY_GOTHIC_18_BOLD
 #define TEMPERATURE_FONT_SIZE         18
-#define TEMPERATURE_TEXT_ADJUSTMENT_X 1
+#define TEMPERATURE_TEXT_ADJUSTMENT_X 2
 #define TEMPERATURE_TEXT_ADJUSTMENT_Y 26
+#define TEMPERATURE_CELCIUS_CONSTANT  273.15
 
 
 static temperature_icon_data *temperature_icon_data_get(TemperatureIcon *aIcon) {
@@ -15,8 +16,8 @@ static temperature_icon_data *temperature_icon_data_get(TemperatureIcon *aIcon) 
 static void draw_degree(GContext *aCtx, GRect r, const char *buffer, int32_t dx, int32_t dy) {
     graphics_draw_text(aCtx, buffer,
         fonts_get_system_font(TEMPERATURE_FONT),
-        GRect(dx + TEMPERATURE_TEXT_ADJUSTMENT_X, (r.size.h - TEMPERATURE_TEXT_ADJUSTMENT_Y) / 2 + dy,
-            r.size.w, TEMPERATURE_FONT_SIZE),
+        GRect(dx, (r.size.h - TEMPERATURE_TEXT_ADJUSTMENT_Y) / 2 + dy,
+            r.size.w + TEMPERATURE_TEXT_ADJUSTMENT_X, TEMPERATURE_FONT_SIZE),
         GTextOverflowModeWordWrap,
         GTextAlignmentCenter,
         NULL);
@@ -24,8 +25,6 @@ static void draw_degree(GContext *aCtx, GRect r, const char *buffer, int32_t dx,
 
 static void paint_temperature_icon(TemperatureIcon *aIcon, GContext *aCtx, GRect r, GPoint aCenter, int32_t aRadius, bool aZoomedIn) {
     int32_t temp = temperature_icon_get_temp(aIcon);
-    // Kelvin => Celcius Degree
-    temp -= 273.15;
     
 // #ifdef PBL_COLOR
 //     graphics_context_set_fill_color(aCtx, GColorMalachite);
@@ -36,7 +35,7 @@ static void paint_temperature_icon(TemperatureIcon *aIcon, GContext *aCtx, GRect
 // #endif
 
     // text
-    static char buffer[] = "-00°";
+    static char buffer[] = "-000°";
     snprintf(buffer, sizeof(buffer)/sizeof(buffer[0]), "%ld°", temp);
 
     graphics_context_set_text_color(aCtx, GColorBlack);
@@ -56,6 +55,10 @@ TemperatureIcon *temperature_icon_create(GRect aFromFrame, GRect aToFrame) {
     TemperatureIcon *icon = icon_create_with_data(aFromFrame, aToFrame,
         sizeof(temperature_icon_data));
     icon_set_painter(icon, paint_temperature_icon);
+    
+    temperature_icon_data *data = temperature_icon_data_get(icon);
+    data->mTemperature = TEMPERATURE_CELCIUS_CONSTANT;
+    
     return icon;
 }
 
@@ -64,7 +67,12 @@ void temperature_icon_destroy(TemperatureIcon *aIcon) {
 }
 
 int32_t temperature_icon_get_temp(TemperatureIcon *aIcon) {
-    return temperature_icon_data_get(aIcon)->mTemperature;
+    int32_t temp = temperature_icon_data_get(aIcon)->mTemperature;
+
+    // Kelvin => Celcius Degree
+    temp -= TEMPERATURE_CELCIUS_CONSTANT;
+
+    return temp;
 }
 
 void temperature_icon_update(TemperatureIcon *aIcon, int32_t aTemperature) {
